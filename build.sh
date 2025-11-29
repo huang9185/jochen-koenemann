973d796e98a57f7a844505d42be1fc975cf477fd
zero_guru <38360323+zerogtiger@users.noreply.github.com>
2025-11-29 16:48:43 -0500

    Moved to Jochen/, build script and readme updated

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
-echo "Building Stockfish"
-cd Stockfish/src
+echo "Building Jochen Koenemann"
+cd Jochen/src
 make build
 make net
-echo "Building Fairy-Stockfish"
-cd ../../Fairy-Stockfish/src
-make build
-make net
-# ./stockfish bench
-
